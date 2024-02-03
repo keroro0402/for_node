2575589ced52af263b56dc46ed5b63d45a0899b8
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 16:43:20 +0900

    顧問追加

--- 040_module_system/start/common.js
+++ 040_module_system/start/common.js
@@ -6,6 +6,10 @@ function minus(a, b) {
   return a - b;
 }
 
+function multi(a, b) {
+  return a * b;
+}
+
 /*
 CJSでモジュールを読み込む時の手順。と言うより必須の文言なので必ず覚えること！！！
  */
@@ -13,4 +17,11 @@ CJSでモジュールを読み込む時の手順。と言うより必須の文�
 module.exports = {
   plus: plus,
   minus: minus,
+  multi, // 関数名とプロパティ名が同じ場合は省略できるので覚えると上級者感が出て良い！！
+};
+
+module.exports = {
+  plus,
+  minus,
+  multi,
 };
--- 040_module_system/start/index.js
+++ 040_module_system/start/index.js
@@ -1,8 +1,11 @@
 // CJSを使ってモジュールを読み込む場合を練習する
 
-let { plus, minus } = require('./common');
+let { plus, minus, multi } = require('./common');
 
 let result = plus(1, 2);
 console.log(result);
 result = minus(1, 2);
+console.log(result);
+
+result = multi(2, 4);
 console.log(result);
