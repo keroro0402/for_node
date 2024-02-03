0ac1b3da6a8bfbb004a5c5687818bf2f54371816
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 13:42:33 +0900

    PRしてみる

--- 030_write_file/start/index.js
+++ 030_write_file/start/index.js
@@ -4,7 +4,7 @@ let fs = require('fs');
 let num = Math.floor(Math.random() * 1e4);
 // fs.writeFileSync('test.txt', 'こんばんは、こんにちは');
 
-// fs.writeFileSync(`test${num}.txt`, `こんにちは、Mr.${num}さん`);
+fs.writeFileSync(`test${num}.txt`, `こんにちは、Mr.${num}さん`);
 
 // console.log(__dirname); // __dirnameは現在実行したscriptまでの絶対パスが記載されている
 
