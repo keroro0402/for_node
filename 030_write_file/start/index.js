d087033c8b4e467c9f095bff37b11d338abc68f6
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 13:20:51 +0900

    ignore追加

--- 030_write_file/start/index.js
+++ 030_write_file/start/index.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-
+// CommonJSでのファイル操作の特徴：requireが使われている
+// CommonJSはNode.jsでしか使えない。requireはnodejsでしが使えない
+let fs = require('fs');
+let num = Math.floor(Math.random() * 1e4);
 fs.writeFileSync('test.txt', 'こんばんは、こんにちは');
+
+fs.writeFileSync(`test${num}.txt`, `こんにちは、Mr.${num}さん`);
 
 console.log(__dirname); // __dirnameは現在実行したscriptまでの絶対パスが記載されている
