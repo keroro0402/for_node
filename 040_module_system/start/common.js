f8619b621ad58f23872b92a29ce905c162e6ae26
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 16:57:32 +0900

    割り算追加

--- 040_module_system/start/common.js
+++ 040_module_system/start/common.js
@@ -10,6 +10,10 @@ function multi(a, b) {
   return a * b;
 }
 
+function division(a, b) {
+  return a / b;
+}
+
 /*
 CJSでモジュールを読み込む時の手順。と言うより必須の文言なので必ず覚えること！！！
  */
@@ -17,11 +21,13 @@ CJSでモジュールを読み込む時の手順。と言うより必須の文�
 module.exports = {
   plus: plus,
   minus: minus,
-  multi, // 関数名とプロパティ名が同じ場合は省略できるので覚えると上級者感が出て良い！！
+  multi,
+  division: division,
 };
 
 module.exports = {
   plus,
   minus,
   multi,
+  division,
 };
--- 040_module_system/start/index.js
+++ 040_module_system/start/index.js
@@ -1,11 +1,13 @@
 // CJSを使ってモジュールを読み込む場合を練習する
 
-let { plus, minus, multi } = require('./common');
+let { plus, minus, multi, division } = require('./common');
 
 let result = plus(1, 2);
-console.log(result);
 result = minus(1, 2);
+result = multi(2, 4);
+result = division(16, 4);
 console.log(result);
 
-result = multi(2, 4);
-console.log(result);
+// pathを書いてみた
+let path = require('path');
+// console.log(path.resolve(__dirname));
