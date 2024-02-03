55716f3844870ba15f7ce5cc7642bb392dc5ef59
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 21:41:20 +0900

    一括でインポートする記述追加

--- 050_module_system_esm/start/MJS/cal.mjs
+++ 050_module_system_esm/start/MJS/cal.mjs
@@ -5,5 +5,14 @@ function sa() {
 function da() {
   console.log('おおげさ');
 }
+
+export function fa() {
+  console.log('gagagagagagaga......');
+}
+
+export default () => {
+  console.log('defaultです！！！');
+};
+
 // ESMではエクスポートする側にはexportを使う
 export { sa, da };
--- 050_module_system_esm/start/MJS/index.mjs
+++ 050_module_system_esm/start/MJS/index.mjs
@@ -1,7 +1,17 @@
 // mjsではインポートする側にはimportを書く
 import { sa } from './cal.mjs';
 import { da } from './cal.mjs';
+import { fa } from './cal.mjs';
+import de from './cal.mjs';
 
-sa();
+import f, * as QW from './cal.mjs'; // 一度に複数のimportする時の記述は * as 任意OBJ名　from "./xxxx.mjs"とする
 
-da();
+console.log(QW);
+
+QW.sa();
+
+QW.da();
+
+QW.fa();
+
+de();
