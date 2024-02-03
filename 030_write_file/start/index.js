eea40d8a8c3e1996a78f5de250426bd95ad3b463
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-03 14:42:16 +0900

    pathモジュール追加

--- 030_write_file/start/index.js
+++ 030_write_file/start/index.js
@@ -1,5 +1,10 @@
 let fs = require('fs');
 
-let num = Math.floor(Math.random() * 1e4);
+let path = require('path');
 
-fs.writeFileSync(__dirname + `/t${num}.txt`, `${num} です。`);
+let num = Math.floor(Math.random() * 1e4);
+let distPath = path.resolve(__dirname, `dist/${num}.txt`);
+
+console.log(distPath);
+
+fs.writeFileSync(distPath, `${num} です。`);
