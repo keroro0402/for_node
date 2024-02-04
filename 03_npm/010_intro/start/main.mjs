676a3a2d5b03d5e4b350429a3c56fa6affca5f0c
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-05 08:52:23 +0900

    npmを使うメリット

--- 03_npm/010_intro/start/main.mjs
+++ 03_npm/010_intro/start/main.mjs
@@ -1,2 +1,14 @@
-const original = { prop: { nested: "value" } };
+// import _ from '../lib/lodash.js'; //npmを使わなかった記述
+import _ from 'lodash'; // npm を使った記述
+
+/**
+ * npm の利点
+ * １：使いたいモジュールやライブラリを一個一個、ダウンロードして階層に配置する手間が省ける
+ */
+
+const original = { prop: { nested: 'value' } };
 // オブジェクトの複製
+const o = { ...original };
+const cloned = _.cloneDeep(original);
+console.log(cloned, cloned === original);
+console.log(o, o === original);
