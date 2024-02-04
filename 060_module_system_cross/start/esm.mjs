9b519ef445ce0c0a03079ebec39f7d483ffb75ca
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-04 14:19:00 +0900

    ESMの時の注意点を書いてみちゃ

---
+++ 060_module_system_cross/start/esm.mjs
@@ -0,0 +1,7 @@
+export function one(data) {
+  return data;
+}
+
+export function two(data) {
+  return 2000000;
+}
--- 060_module_system_cross/start/example.mjs
+++ 060_module_system_cross/start/example.mjs
@@ -1,3 +1,6 @@
+// 拡張子をmjsにする利点：package.jsonにESMを明示しなくてもCJSで管理されたモジュールが読み込むことが出来る。
+// 拡張子をmjsにしない場合、ESMを使うには必ずpackage.jsonにtypeプロパティでmoduleを記載しないと使えないので注意！！
+
 import * as SEX from './esm.mjs'; // ESMで読みこむ
 import { Chi } from './calc.cjs'; // CJSで書かれたモジュールもimportで読み込める！！
 /* require（CJS）では読み込めない！！！ので、使うなよ！！！ */
