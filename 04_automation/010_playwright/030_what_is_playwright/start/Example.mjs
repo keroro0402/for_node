3c8ff01ff730c176ad5e572e11b756cf1cad9dc2
keroro0402 <otsuyu0402@yahoo.co.jp>
2024-02-25 21:35:20 +0900

    学びおなpし

--- 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
+++ 04_automation/010_playwright/030_what_is_playwright/start/Example.mjs
@@ -1,10 +1,29 @@
 import { chromium } from '@playwright/test';
 
+// console.log(chromium);
+
 (async () => {
   const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
+
+  console.dir(page);
+
   await page.goto('http://localhost:3000/');
-  const htmlStr = await page.content();
-  console.log(htmlStr);
-  await browser.close();
+
+  // css セレクター
+  let getTitleLocater = await page.locator('.navbar-brand');
+  let pageTitle = await getTitleLocater.innerText();
+  console.log(pageTitle);
+
+  // 文字列で要素を取得
+  let textLocater = await page.locator('text=名刺管理アプリ');
+  pageTitle = await textLocater.innerText();
+  console.log(pageTitle);
+
+  // xpathで要素を取得
+  let xpathLocater = await page.locator('//*[@id="__next"]/nav/div/a');
+  pageTitle = await xpathLocater.innerText();
+  console.log(pageTitle);
+
+  //await browser.close();
 })();
